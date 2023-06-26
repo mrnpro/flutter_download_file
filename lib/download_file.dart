029874fbf9c6802ae5fdcea59086d895c5a76edc
Marco Musiol <0203marcom@gmail.com>
2023-06-26 12:15:16 +0200

    Added download method & packages

--- lib/download_file.dart
+++ lib/download_file.dart
@@ -1,7 +1,19 @@
 library download_file;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class DownloadFile {
+  /// Download the file from the given [downloadUrl]
+  /// 
+  /// The [fileName] is the name how the file gets saved. For example "image.png"
+  ///
+  /// Additionally you can add a [savePath] here if you want to save the file in a 
+  /// subfolder. You can use "documents/images" here.
+  static Future<void> downloadAndSafeFile({
+    required String downloadUrl,
+    required String fileName,
+    String? savePath,
+  }) async {
+
+    
+
+  }
 }
