c63c25b3adce62700691ea4ca2dc6f171d9c59b2
Marco Musiol <0203marcom@gmail.com>
2023-06-28 14:29:04 +0200

    Passing download arguments

--- lib/download_file.dart
+++ lib/download_file.dart
@@ -22,13 +22,29 @@ class DownloadFile {
   ///
   /// Additionally you can add a [savePath] here if you want to save the file in a
   /// subfolder. You can use "documents/images" here.
+  ///
+  /// The [loadingWidget] widget that is displaying while the download is loading
+  /// If no one is passed it's using the default one
+  ///
+  /// The [customDialogContent] widget will replace the complete [AlertDialog] content
+  /// If you pass this [Widget] you don't need to pass a custom [loadingWidget],
+  /// because everything will be overwritten by this anyway
+  ///
+  /// The [errorMessage] is displayed if an error occurs
+  /// If no text is passed it will use the default one
   static Future<void> downloadAndSafeFile({
     required DownloadFileOptions downloadFileOptions,
     required BuildContext context,
+    String? errorMessage,
+    Widget? loadingWidget,
+    Widget? customDialogContent,
   }) async {
     showDialog(
       context: context,
       builder: (_) => DownloadFileDialog(
+        customDialogContent: customDialogContent,
+        errorMessage: errorMessage,
+        loadingWidget: loadingWidget,
         downloadFileOptions: downloadFileOptions,
       ),
     );
